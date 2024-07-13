9780bf96f1ba510ee13e754399c9afb1597a5eaa
Shreya Sanjay <shreyasanjay.24@gmail.com>
2024-07-13 09:25:40 +0530

    done all activities of day1

---
+++ Day01_variables-and-datatypes/index.js
@@ -0,0 +1,37 @@
+// Activity 1
+var myAge=18;
+console.log(myAge)
+
+let myName='Quokka'
+console.log(myName)
+
+// Activity 2
+const isLoggedIn=true
+console.log(isLoggedIn)
+
+// Activity 3
+aNum=12
+console.log(typeof(aNum))
+
+aString='hi'
+console.log(typeof(aString))
+
+aBool=false
+console.log(typeof(aBool))
+
+anArr=[1,2,3,4,5]
+console.log(typeof(anArr))
+
+anObj={rollNo:'1',name:'shre',email:'shre@google.co'}
+console.log(typeof(anObj))
+
+// Activity 4
+let num=21
+console.log(num)
+num=25
+console.log(num)
+
+// Activity 5
+const pi=3.14
+console.log(pi)
+// pi=4.0 //error: Assignment to constant variable
