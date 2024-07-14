b36bb1d2005460ddd2e10b372b39ed4ecb9557da
Shreya Sanjay <shreyasanjay.24@gmail.com>
2024-07-14 11:06:05 +0530

    operators done

---
+++ Day02_operators/index.js
@@ -0,0 +1,71 @@
+// Activity One
+
+// Task 1
+n1=10;
+n2=20;
+console.log(`sum = ${n1+n2}`)
+// Task 2
+console.log(`difference = ${n1-n2}`)
+// Task 3
+console.log(`product = ${n1*n2}`)
+// Task 4
+console.log(`quotient = ${n1/n2}`)
+// Task 5
+console.log(`remainder = ${n1%n2}`)
+
+// Activity 2
+
+// Task 6
+n3=45
+n3+=5
+console.log(`new no = ${n3}`)
+// Task 7
+n4=75
+n4-=5
+console.log(`new no = ${n4}`)
+
+// Activity 3
+
+// Task 8
+myAge=18
+yourAge=24
+console.log(myAge>yourAge)
+console.log(myAge<yourAge)
+// Task 9
+a=15
+b=20
+console.log(a>=b)
+console.log(a<=b)
+// Task 10
+i=10 
+j=20
+k=10
+console.log(i==j)
+console.log(i==k)
+console.log(i===j)
+console.log(i===k)
+
+// Activity 4
+
+// Task 11
+let x= 5
+let y= 10
+andOperation= (x===y&&y===x)
+console.log(`and operation ${andOperation}`)
+// Task 12
+let orOperation= (x===y||y===x)
+console.log(`or operation ${orOperation}`)
+// Task 13
+let notOperation= (x!=y)
+console.log(`not operation ${notOperation}`)
+
+// Activity 5
+
+// Task 14
+const num1=45
+let result1=0>num1?"negative":"positive";
+console.log(`${result1}`)
+
+const num2=-90
+let result2=0>num2?"negative":"positive";
+console.log(`${result2}`)
