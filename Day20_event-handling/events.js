2b4f84e14e00dd38b601f3e81fcbfd8015e7a202
Shreya Sanjay <shreyasanjay.24@gmail.com>
2024-07-22 09:52:58 +0530

    activity 3 done

--- Day20_event-handling/events.js
+++ Day20_event-handling/events.js
@@ -12,3 +12,32 @@ const img1=document.getElementById('image');
 btn2.addEventListener("dblclick",()=>{
     img1.hidden=!img1.hidden;
 });
+
+
+// Activity 2
+// Task 3
+const h1=document.getElementById('heading1');
+h1.addEventListener("mouseover",()=>{
+    h1.style.backgroundColor="green";
+});
+
+// Task 4
+h1.addEventListener("mouseout",()=>{
+    h1.style.backgroundColor="";
+});
+
+
+// Activity 3
+// Task 5
+const inputField=document.getElementById('inputField');
+inputField.addEventListener("keydown",(event)=>{
+    console.log(`Key Pressed: ${event.key}`);
+});
+
+// Task 6
+const input=document.getElementById('input');
+const para2=document.getElementById('displayParagraph');
+input.addEventListener("keyup",()=>{
+    para2.textContent=input.value;
+});
+
