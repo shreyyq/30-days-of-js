45bfa3bce257b07a300616a8dcf91fe8f673ed48
Shreya Sanjay <shreyasanjay.24@gmail.com>
2024-07-23 10:25:39 +0530

    day11 done

--- Day11_promises-async-await/index.js
+++ Day11_promises-async-await/index.js
@@ -62,3 +62,106 @@ userData
 
 // Activity 3
 // Task 4
+const promiseThree=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error=false;
+        if(!error){
+            resolve({username:"javascript", password:123})
+        }
+        else{
+            reject(`ERROR in Promise Four`)
+        }
+    },2000)
+});
+
+async function consumePromiseThree(){
+        const response=await promiseThree
+        console.log(response);
+}
+
+consumePromiseThree();
+
+// Task 5
+const promiseFour=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error=true;
+        if(!error){
+            resolve({username:"java", password:123})
+        }
+        else{
+            reject(`ERROR`)
+        }
+    },2000)
+});
+
+async function consumePromiseFour(){
+    try{
+        const response=await promiseFour
+        console.log(response);
+    }
+    catch (error){
+        console.log(error);
+    }
+}
+
+consumePromiseFour();
+
+
+// Activity 4
+// Task 6
+fetch (`https://api.github.com/users/shreyyq`)
+.then((response)=>{
+    return response.json();
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=> console.log(error));
+
+// Task 7
+async function fetchPosts() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching posts:', error);
+    }
+}
+
+fetchPosts();
+
+
+// Activity 5
+// Task 8
+const bulbasaur_pokemons_url='https://pokeapi.co/api/v2/pokemon/bulbasaur';
+const raticate_pokemons_url='https://pokeapi.co/api/v2/pokemon/raticate';
+const kakuna_pokemons_url='https://pokeapi.co/api/v2/pokemon/kakuna';
+
+async function getPromise(url){
+    return fetch(url).then(response=>{
+        if(!response.ok){
+            throw new Error(`Network response was not ok`);
+        }
+        return response.json();
+    });
+}
+
+let promise1=getPromise(bulbasaur_pokemons_url);
+let promise2=getPromise(raticate_pokemons_url);
+let promise3=getPromise(kakuna_pokemons_url);
+
+Promise.all([promise1,promise2,promise3]).then(result=>{
+    console.log({result});
+})
+.catch(error=>{
+    console.log('An Error Occurred');
+});
+
+// Task 9
+Promise.race([promise1,promise2,promise3]).then(result=>{
+    console.log(JSON.parse(result));
+})
+.catch(error=>{
+    console.log('An Error Occurred');
+});
