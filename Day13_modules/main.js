ad7aed7ee88a870ecbc9b96224c1e9ad3419131b
Shreya Sanjay <shreyasanjay.24@gmail.com>
2024-07-25 08:24:56 +0530

    task1 done

---
+++ Day13_modules/main.js
@@ -0,0 +1,6 @@
+// Activity 1
+// Task 1
+import { add } from "./utility.js";
+console.log(add(2,3));
+
+// Task 2
---
+++ Day13_modules/utility.js
@@ -0,0 +1,11 @@
+// Task 1
+export function add(n1,n2){
+    return n1+n2;
+}
+
+// Task 2
+export const person={
+    name:'javascript',
+    isLoggedIn:true,
+    age:78
+};
