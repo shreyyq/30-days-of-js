21ce1c21a4c0adbefb0058aba3c29fe9cb192586
Shreya Sanjay <shreyasanjay.24@gmail.com>
2024-07-25 10:35:51 +0530

    activity 2 done

--- Day13_modules/main.js
+++ Day13_modules/main.js
@@ -1,6 +1,29 @@
 // Activity 1
 // Task 1
-import { add } from "./utility.js";
-console.log(add(2,3));
+import {adds} from "./utility.js";
+console.log(adds(2,3));
 
 // Task 2
+import { person } from "./utility.js";
+console.log(person.greet());
+console.log(person.celebrateBday());
+console.log(person.greet());
+
+
+// Activity 2
+// Task 3
+import {add,subtract,multiply,divide} from './utility.js';
+
+console.log('Addition:',add(5,3));
+console.log('Subtraction:',subtract(5,3));
+console.log('Multiply:',multiply(5,3));
+try{
+    console.log('Division:',divide(5,0));
+}
+catch(error){
+    console.error(error.message);
+};
+
+// Task 4
+import welcome from "./utility.js";
+console.log(welcome('Shreya'));
--- Day13_modules/utility.js
+++ Day13_modules/utility.js
@@ -1,11 +1,49 @@
+// Activity 1
 // Task 1
-export function add(n1,n2){
+export function adds(n1,n2){
     return n1+n2;
 }
 
 // Task 2
-export const person={
+const person={
     name:'javascript',
     isLoggedIn:true,
-    age:78
+    age:30,
+    greet: function(){
+    return `Hello,my name is ${this.name} and age is ${this.age}.`;
+    },
+    celebrateBday: function(){
+        this.age+=1;
+        return `Happy Birthday! I am ${this.age} years old.`;
+    }
 };
+export {person};
+
+
+// Activity 2
+// Task 3
+export function add(a,b){
+    return a+b;
+}
+export function subtract(a,b){
+    return a-b;
+}
+export function multiply(a,b){
+    return a*b;
+}
+export function divide(a,b){
+    if(b===0){
+        throw new Error('Division by zero is not allowed');
+    }
+    return a/b;
+};
+
+// Task 4
+export default function welcome(name){
+    return `Welcome to JS Modules, ${name}`;
+};
+
+
+// Activity 3
+// Task 5
+
