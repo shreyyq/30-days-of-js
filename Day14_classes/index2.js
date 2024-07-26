3e97ed461e1df9a252c45d52cab74f11e1db4fa7
Shreya Sanjay <shreyasanjay.24@gmail.com>
2024-07-26 10:17:22 +0530

    day14 done

--- Day14_classes/index2.js
+++ Day14_classes/index2.js
@@ -24,5 +24,44 @@ console.log(`Hello! ${a.firstName} ${a.lastName}`);
 // Activity 5
 // Task 9
 class Account{
-    
-}
+    #balance;
+    constructor(initialBalance=0){
+        this.#balance=initialBalance;
+    }
+
+    deposit(amount){
+        if(amount>0){
+            this.#balance+=amount;
+            console.log(`Deposited: $${amount}. New Balance: $${this.#balance}`);
+        }
+        else{
+            console.log(`Deposit amount must be valid`);
+        }
+    }
+
+    withdraw(amount){
+        if(amount>0 && amount<=this.#balance){
+            this.#balance-=amount;
+            console.log(`Withdrew: $${amount}. New Balance: $${this.#balance}`);
+        }
+        else if(amount>this.#balance){
+            console.log(`Insufficient Balance`);
+        }
+        else{
+            console.log(`Withdrawal amount must be valid`);
+        }
+    }
+
+    getBalance(){
+        return this.#balance;
+    }
+};
+
+// Task 10
+const myAccount=new Account(100);
+myAccount.deposit(50);
+myAccount.withdraw(30);
+myAccount.withdraw(200);
+myAccount.deposit(-20);
+myAccount.deposit(-10);
+console.log(`Final balance: ${myAccount.getBalance()}`);
