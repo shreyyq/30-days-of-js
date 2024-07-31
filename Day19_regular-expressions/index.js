194d1b95231aa89ac67b05aa1547841a8fae0b9e
Shreya Sanjay <shreyasanjay.24@gmail.com>
2024-07-31 12:50:55 +0530

    day19 done

--- Day19_regular-expressions/index.js
+++ Day19_regular-expressions/index.js
@@ -61,3 +61,47 @@ else{
 
 // Activity 4
 // Task 7
+const text6=`Hello World! This is a test`;
+const regex7=/^\w+/;
+const matches7=text6.match(regex7);
+if(matches7){
+    console.log(`Match:`,matches7[0]);
+}
+else{
+    console.log(`No match Found`);
+};
+
+// Task 8
+const text7=`Hello World! This is a test`;
+const regex8=/\b\w+\b$/;
+const matches8=text7.match(regex8);
+if(matches8){
+    console.log(`Match:`,matches8[0]);
+}
+else{
+    console.log(`No match Found`);
+};
+
+
+// Activity 5 //ToDo:REVISE!!
+// Task 9 
+const password=`Password1!`;
+const regex9=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+[\]{};':",.<>/?])(?!.*\s).{8,}$/;
+const matches9=password.match(regex9);
+if(matches9){
+    console.log(`Password is valid!`);
+}
+else{
+    console.log(`Password is Invalid!`);
+};
+
+// Task 10
+const url=`https://www.example.com/path/to/resource?query=string#fragment`;
+const regex10=/^(https?:\/\/)?(www\.)?[\w-]+\.[a-z]{2,}(\/[\w-]*)*(\?[;&a-z=\d%_]*)?(\#[\w-]*)?$/i;
+const matches10=url.match(regex10);
+if(matches10){
+    console.log(`Valid URL!`);
+}
+else{
+    console.log(`Invalid URL!`);
+};
