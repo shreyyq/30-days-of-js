5f3d9c622a5490cafd8c5e03166202336eda9af3
Shreya Sanjay <shreyasanjay.24@gmail.com>
2024-08-02 11:17:30 +0530

    day21 done

--- Day21_leetcode-easy/index.js
+++ Day21_leetcode-easy/index.js
@@ -56,3 +56,82 @@ console.log(palindromeNo(32125));
 
 
 // Activity 4 (Merge Two Sorted Lists)
+function mergeTwoLists(list1,list2){
+    let dummy=new ListNode(-1);
+    let head=dummy;
+
+    while(list1&&list2){
+        if(list1.val<list2.val){
+            head.next=list1;
+            list1=list1.next;
+        }
+        else{
+            head.next=list2;
+            list2=list2.next;
+        }
+        head=head.next;
+    }
+    head.next=list1||list2;
+
+    return dummy.next;
+    }
+class ListNode{
+    constructor(val=0,next=null){
+        this.val=val;
+        this.next=next;
+    }
+}
+
+const list1=new ListNode(1);
+list1.next=new ListNode(2);
+list1.next.next=new ListNode(4);
+
+const list2=new ListNode(1);
+list2.next=new ListNode(3);
+list2.next.next=new ListNode(4);
+
+const mergedList=(mergeTwoLists(list1,list2));
+
+function printList(list){
+    let current=list;
+    let result='';
+    while(current){
+        result+=current.val+'->';
+        current=current.next;
+    }
+    result+=null;
+    console.log(result);
+}
+printList(mergedList);
+
+
+// Activity 5 (Valid Parentheses)
+function validParentheses(str){
+    let stack=[];
+    let map={
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
+    for(let i=0;i<str.length;i++){
+        if(str[i]==='('|| str[i]==='['|| str[i]==='{'){
+            stack.push(str[i]);
+        }
+        else{
+            let last=stack.pop();
+            if(str[i]!==map[last]){
+                return false;
+            }
+        }
+    }
+    if(stack.length!==0){
+        return false;
+    }
+    return true;
+};
+
+console.log(validParentheses('()'));
+console.log(validParentheses('()[]{}'));
+console.log(validParentheses('(]'));
+console.log(validParentheses('([)]'));
+console.log(validParentheses('{[]}'));
